b97221a3504b17a7fa39f29b4e3eebdc4d6d2991
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-06-19 10:36:12 +0530

    changes in the printing
    changed to sarvesh and hey brother

---
+++ src/JAVABASICS/basics.java
@@ -0,0 +1,9 @@
+package JAVABASICS;
+
+public class basics {
+    public static void main(String[] args) {
+        System.out.println("sarveshruns");
+        System.out.print("hey brother");
+        System.out.println("sarvesh"); ");
+    }
+}
