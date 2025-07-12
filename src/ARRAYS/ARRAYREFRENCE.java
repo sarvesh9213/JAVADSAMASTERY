c808ae8b0728b1b9af92ff08775f88480ec2c582
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-07-12 11:19:03 +0530

    create reference array in java
    Added Java example to demonstrate reference copying of arrays
    
    - Created a printArray() function to display array contents
    - Initialized an array with values {5, 6, 7, 8, 9}
    - Assigned the array to another variable (reference copy)
    - Modified elements in the copied array
    - Showed that changes reflect in the original array due to shared reference
    - Explained output with comments for better understanding

---
+++ src/ARRAYS/ARRAYREFRENCE.java
@@ -0,0 +1,38 @@
+package ARRAYS;
+import java.util.*;
+
+public class ARRAYREFRENCE {
+
+    // Function to print array elements
+    static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println(); // For a new line after printing array
+    }
+
+    public static void main(String[] args) {
+        // Original array with values
+        int[] arr = {5, 6, 7, 8, 9};
+
+        // Attempting to copy arr to arr2 (Note: this is a reference copy)
+        int[] arr2 = arr;
+
+        // Print copied array
+        System.out.println("Copied Array:");
+        printArray(arr2);
+
+        // Changing some values in arr2
+        arr2[0] = 0;
+        arr2[1] = 0;
+
+        // Print original array after changes
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        // Print arr2 after modification
+        System.out.println("Modified Copied Array:");
+        printArray(arr2);
+    }
+}
+
