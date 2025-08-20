9867de492e1e97b9ebf89431150fefcac659f15c
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-08-20 13:21:26 +0530

    Create twoppointer_even_odd.java
    int[] arr1 = {1, 0, 1, 0, 1, 0};
    sortZeroesAndOneInPlace(arr1);
    // Output: 0 0 0 1 1 1
    
    int[] arr2 = {3, 1, 2, 4, 7, 6};
    sortArrayByParity(arr2);
    // Output: 6 4 2 1 7 3   (Evens first, odds later)

---
+++ src/ARRAYS/Lect_5/twoppointer_even_odd.java
@@ -0,0 +1,43 @@
+package ARRAYS.Lect_5;
+public class twoppointer_even_odd {
+
+    static void sortArrayByParity(int[] arr) {
+        int n = arr.length;
+        int left = 0, right = n - 1;
+
+        while (left < right) {
+            // Case 1: left is odd & right is even -> swap
+            if (arr[left] % 2 == 1 && arr[right] % 2 == 0) {
+                swap(arr, left, right);
+                left++;
+                right--;
+            }
+            // Case 2: if left is already even -> move forward
+            else if (arr[left] % 2 == 0) {
+                left++;
+            }
+            // Case 3: if right is already odd -> move backward
+            else if (arr[right] % 2 == 1) {
+                right--;
+            }
+        }
+    }
+
+    // helper swap function
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // test function
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 2, 4, 7, 6, 9, 8};
+        sortArrayByParity(arr);
+
+        System.out.print("Sorted by Parity: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
