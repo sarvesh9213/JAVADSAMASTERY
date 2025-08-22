215a803af0f4cebbfe16bd7cd074586ddfb833b3
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-08-22 21:23:16 +0530

    Update SortZeroOne.java

--- src/ARRAYS/Lect_5/SortZeroOne.java
+++ src/ARRAYS/Lect_5/SortZeroOne.java
@@ -1,34 +1,29 @@
-package ARRAYS.Lect_5;
 public class SortZeroOne {
     public static void main(String[] args) {
-        int[] arr = {0, 1, 0, 1, 1, 0, 0, 1};
-
+        int[] arr = {1, 0, 0, 1, 0, 1, 1, 0, 0};
         int n = arr.length;
-        int zeroes = 0;
 
-        // Count the number of zeroes
+        // Step 1: Count zeros
+        int zeroCount = 0;
         for (int i = 0; i < n; i++) {
             if (arr[i] == 0) {
-                zeroes++;
+                zeroCount++;
             }
         }
 
-        // Fill the array: first zeroes are 0, rest are 1
+        // Step 2: Fill array using the red box logic
         for (int i = 0; i < n; i++) {
-            if (i < zeroes) {
+            if (i < zeroCount) {
                 arr[i] = 0;
             } else {
                 arr[i] = 1;
             }
         }
 
-        // Print the sorted array
-        System.out.print("Sorted: ");
-        for (int j : arr) {
-            System.out.print(j + " ");
+        // Print sorted array
+        System.out.print("Sorted Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
         }
     }
 }
-//"C:\Program Files\Java\jdk-24\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2025.1.2\lib\idea_rt.jar=64983" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath C:\Users\sarve\IdeaProjects\JAVADSAMASTERY\out\production\JAVADSAMASTERY ARRAYS.Lect_5.SortZeroOne
-//Sorted: 0 0 0 0 1 1 1 1
-//Process finished with exit code 0
