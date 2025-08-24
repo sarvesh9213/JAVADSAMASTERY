e730af6e0fac0ee3613a958879d84dd709d7da39
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-08-25 00:07:05 +0530

    Update basicsll.java
    how to link linked list

--- src/linkedlist/lect1/basicsll.java
+++ src/linkedlist/lect1/basicsll.java
@@ -1,15 +1,29 @@
 package linkedlist.lect1;
 
 public class basicsll {
+    // Static inner class Node
     public static class Node {
-    int data ; // stores the actual value
-    Node next ; // stores the address of the next node
+        int data;     // stores the actual value
+        Node next;    // stores the address of the next node
+
+        Node(int data) {
+            this.data = data;
+            // Ye constructor sirf Node banate hi uske andar data store kar deta hai, jisse code clean aur short ho jaata hai.
+        }
     }
 
     public static void main(String[] args) {
-        Node x = new Node() ;
-        System.out.println(x); // linkedlist.lect1.basicsll$Node@30f39991
-        System.out.println(x.data); //0
-        System.out.println(x.next); //null
+        Node a = new Node(5);
+        Node b = new Node(3);
+        Node c = new Node(9);
+        Node d = new Node(58);
+        Node e = new Node(16);
+
+        // Example: linking nodes manually
+        a.next = b; // stores the address of the next node
+        b.next = c;
+        c.next = d;
+        d.next = e;
+
     }
 }
