8d497d7233e14ead6ec69e579fd50716607f197b
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-09-18 01:32:47 +0530

    Create switchcase.java

---
+++ src/JAVABASICS/switchcase.java
@@ -0,0 +1,42 @@
+package JAVABASICS;
+import java.util.Scanner;
+
+public class switchcase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number (1-7): ");
+        int day = sc.nextInt();
+
+        String dayName;
+
+        switch (day) {
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            case 4:
+                dayName = "Thursday";
+                break;
+            case 5:
+                dayName = "Friday";
+                break;
+            case 6:
+                dayName = "Saturday";
+                break;
+            case 7:
+                dayName = "Sunday";
+                break;
+            default:
+                dayName = "Invalid input! Please enter 1–7.";
+        }
+
+        System.out.println("Day: " + dayName);
+        sc.close();
+    }
+}
