36f1c452218064ede1a18f40045a26d0845ec4bc
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-09-21 13:09:16 +0530

    Create person.java

---
+++ src/oopslect11_12_13/person.java
@@ -0,0 +1,8 @@
+package oopslect11_12_13;
+
+public class person {
+    int age = 20 ;
+    public static void main(String[] args) {
+    person Sarvesh = new person(); // yaha per hum apna ek object bena rhe hai 
+    }
+}
