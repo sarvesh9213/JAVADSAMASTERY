f272972263c83c71872e9ee2a5f5dae02190c2a0
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-09-26 01:15:05 +0530

    Create maths.java

---
+++ src/oopslect11_12_13/lecture_11/lecture_12/maths.java
@@ -0,0 +1,16 @@
+package oopslect11_12_13.lecture_11.lecture_12;
+public class maths {
+    public static void main(String[] args) {
+
+        // Square root
+        System.out.println("Square root of 16: " + Math.sqrt(16));
+
+        // Floor values
+        System.out.println("Floor of 5.3: " + Math.floor(5.3));
+        System.out.println("Floor of 5.8: " + Math.floor(5.8));
+
+        // Ceil values
+        System.out.println("Ceil of 5.3: " + Math.ceil(5.3));
+        System.out.println("Ceil of 5.8: " + Math.ceil(5.8));
+    }
+}
