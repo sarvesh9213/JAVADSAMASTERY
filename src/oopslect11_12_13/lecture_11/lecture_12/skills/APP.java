7ff3f55c91c6d8e537ac68a36694fbfd61a1bcdc
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-10-10 12:11:13 +0530

    Update APP.java

--- src/oopslect11_12_13/lecture_11/lecture_12/skills/APP.java
+++ src/oopslect11_12_13/lecture_11/lecture_12/skills/APP.java
@@ -1,6 +1,18 @@
 package oopslect11_12_13.lecture_11.lecture_12.skills;
 
 public class APP {
+
+    public String str_1 = "i am a public member " ;
+
+    void printFromClass(){
+
+        System.out.println("within class : "+ str_1);
+}
+    public static void main(String[] args) {
+    APP obj1 = new APP() ;
+    obj1.printFromClass();
+        System.out.println(obj1.str_1);
+    }
 }
 class APP2 {
 
