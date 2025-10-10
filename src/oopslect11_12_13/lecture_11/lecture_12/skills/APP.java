6741e4bf1eccbccf64aa034c2e44a6bdea24c17a
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2025-10-10 10:49:08 +0530

    class outside the package

---
+++ src/oopslect11_12_13/lecture_11/lecture_12/skills/APP.java
@@ -0,0 +1,7 @@
+package oopslect11_12_13.lecture_11.lecture_12.skills;
+
+public class APP {
+}
+class APP2 {
+
+}
---
+++ src/outsidePWSKILLS.java
@@ -0,0 +1,6 @@
+
+import oopslect11_12_13.lecture_11.lecture_12.skills.APP;
+
+public class outsidePWSKILLS {
+    APP obj = new APP();
+}
